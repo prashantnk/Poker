e9379ae7e33aaf4486ab48d75e198d291d0ac85f
prashantnk <72505882+prashantnk@users.noreply.github.com>
2026-03-08 14:26:23 +0000

    add analytics

--- poker-game/app/layout.tsx
+++ poker-game/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"; // <-- 1. Import Analytics
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics /> {/* <-- 2. Add Component here */}
       </body>
     </html>
   );
